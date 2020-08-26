c8bc027a9e199df7dfe2e92afa0685489b468ebb
lnguyen193 <lynn.clover1206@gmail.com>
2020-08-25 23:13:17 -0400

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int testScore = 50;
+        System.out.println("Test Score initialized is: " + testScore);
+
+        double priceTag = 24.99;
+        System.out.println("Price tag: " + priceTag);
+
+        String userName = "Bobby Johnson";
+        System.out.println("Username: " + userName);
+
+    }
+}
